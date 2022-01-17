2cbea7f1952f7898b5af7449a8c0afae926e2ebe
hrqcds <hrqcds.dev@gmail.com>
2022-01-16 22:49:03 -0400

    primeiro desafio elixir

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: list_length(list, 0)
+
+  def call_enum(list), do: length(list)
+
+  defp list_length([], acc), do: acc
+
+  defp list_length([head | tail], acc) do
+    acc = acc + 1
+
+    list_length(tail, acc)
   end
 end
